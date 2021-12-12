23404a8d59f0a3a18f5c864e23169b4be7ebac76
Alexey <alexey.chukhryaev@gmail.com>
2021-12-12 11:24:47 +0100

    Events facase example

--- events/api.py
+++ events/api.py
@@ -1,6 +1,10 @@
+from events.facade import Facade
+
+
 def extend_application(app):
     @app.route('/events')
     def events_handler():
+        Facade.get_event(1)
         return 'Get events handler'
 
     @app.route('/event')
